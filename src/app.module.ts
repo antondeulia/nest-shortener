d5440816096ef54f8f7db137ada0fe3d3918a517
Anton Deulia <anton.deulia@kauz.net>
2024-11-02 00:13:19 +0100

    small fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { PrismaModule } from 'prisma/prisma.module'
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ShortenedUrlsModule } from './shortened-urls/shortened-urls.module';
+import { PrismaModule } from '../prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { ShortenedUrlsModule } from './shortened-urls/shortened-urls.module'
 
 @Module({
-	imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, ShortenedUrlsModule]
+	imports: [
+		PrismaModule,
+		ConfigModule.forRoot({ isGlobal: true }),
+		AuthModule,
+		UsersModule,
+		ShortenedUrlsModule
+	]
 })
 export class AppModule {}
